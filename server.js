6a404ee7f2bc29981537d3ce4e700e2ac38623f6
Rajat <rajat804@gmail.com>
2026-05-15 18:33:45 +0530

    server update

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const app = express();
 ================================ */
 const allowedOrigins = [
   "https://ashtro-seven.vercel.app",
+  "https://astroplanets.vercel.app",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://astroplanets.co.in",
