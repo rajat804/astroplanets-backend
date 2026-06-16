1f9a2179b9ae9a3f9ca11baec678b35f46f3356e
Rajat <rajat804@gmail.com>
2026-06-16 19:27:43 +0530

    social content rotes

--- routes/socialContentRoutes.js
+++ routes/socialContentRoutes.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getAllContent,
-  getContentByType,
-  createContent,
-  updateContent,
-  deleteContent,
-  incrementViews,
-  incrementLikes,
-} = require('../controllers/socialContentController');
 const { protectAdmin } = require('../middleware/adminAuth');
+const { protect } = require('../middleware/auth');
+const { upload } = require('../config/cloudinary'); // Make sure this path is correct
+const {
+  getAllSocialContent,
+  getActiveSocialContent,
+  getSocialContentById,
+  createSocialContent,
+  uploadFile,
+  updateSocialContent,
+  deleteSocialContent,
+  incrementViewCount,
+  toggleLike
+} = require('../controllers/socialContentController');
 
-// Public routes
-router.get('/', getAllContent);
-router.get('/type/:type', getContentByType);
-router.put('/:id/view', incrementViews);
-router.put('/:id/like', incrementLikes);
+// Public routes (for frontend)
+router.get('/active', getActiveSocialContent);
+router.get('/:id', getSocialContentById);
+router.put('/:id/view', incrementViewCount);
+router.put('/:id/like', toggleLike);
 
 // Admin routes
-router.post('/admin', protectAdmin, createContent);
-router.put('/admin/:id', protectAdmin, updateContent);
-router.delete('/admin/:id', protectAdmin, deleteContent);
+router.get('/', protectAdmin, getAllSocialContent);
+router.post('/', protectAdmin, createSocialContent);
+router.post('/upload', protectAdmin, upload.single('file'), uploadFile);
+router.put('/:id', protectAdmin, updateSocialContent);
+router.delete('/:id', protectAdmin, deleteSocialContent);
 
 module.exports = router;
