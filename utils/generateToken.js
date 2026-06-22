adb804aea24849ad1a37a1fcb1e8005e445c989d
Rajat <rajat804@gmail.com>
2026-06-22 21:04:44 +0530

    generate token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
+   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 };
 
 module.exports = generateToken;
